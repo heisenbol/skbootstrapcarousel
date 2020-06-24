4e179a1c39704f837be9ffe1cd5a57e26a1989a2
Stefanos Karasavvidis <sk@karasavvidis.gr>
2020-06-24 08:37:40 +0300

    more TYPO3 compatibility

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,37 @@
 <?php
 defined('TYPO3_MODE') || die();
+$boot = function () {
+
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['skbootstrapcarousel_skbootstrapcarousel'] =
+	   \Skar\Skbootstrapcarousel\Hooks\PageLayoutView\PreviewRenderer::class;
 
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['skbootstrapcarousel_skbootstrapcarousel'] =
-   \Skar\Skbootstrapcarousel\Hooks\PageLayoutView\PreviewRenderer::class;
+	$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+
+	$iconRegistry->registerIcon(
+		'skbootstrapcarousel-icon',
+		\TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+		['source' => 'EXT:skbootstrapcarousel/Resources/Public/Images/Backend/ContentElement/Carousel.png']
+	);
+	
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+	mod {
+	    wizards.newContentElement.wizardItems.special {
+	        elements {
+	            skbootstrapcarousel {
+	                iconIdentifier = skbootstrapcarousel-icon
+	                title = SK Bootstrap image and content carousel
+	                description = Versatile image and content carousel for Bootstrap 4
+	                tt_content_defValues {
+	                    CType = skbootstrapcarousel
+	                }
+	            }
+	        }
+	        show = *
+	    }
+	}
+	');
+};
+
+$boot();
+unset($boot);
--- ext_tables.php
+++ ext_tables.php
@@ -1,38 +1,19 @@
 <?php
 	defined('TYPO3_MODE') || die('Access denied.');
 
+$boot = function () {
 
 
 
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('skbootstrapcarousel', 'Configuration/TypoScript', 'SK Bootstrap Carousel');
 
 	/***************
 	 * Allow Custom Records on Standard Pages
 	 */
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_skbootstrapcarousel_carousel_item');
 
-	$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
 
-	$iconRegistry->registerIcon(
-		'skbootstrapcarousel-icon',
-		\TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-		['source' => 'EXT:skbootstrapcarousel/Resources/Public/Images/Backend/ContentElement/Carousel.png']
-	);
 
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-	mod {
-	    wizards.newContentElement.wizardItems.special {
-	        elements {
-	            skbootstrapcarousel {
-	                iconIdentifier = skbootstrapcarousel-icon
-	                title = SK Bootstrap image and content carousel
-	                description = Versatile image and content carousel for Bootstrap 4
-	                tt_content_defValues {
-	                    CType = skbootstrapcarousel
-	                }
-	            }
-	        }
-	        show = *
-	    }
-	}
-	');
+};
+
+$boot();
+unset($boot);
